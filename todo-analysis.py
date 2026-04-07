ec0e7be8238b6b3e47482ace5613104121275030
will <willschetelich@gmail.com>
2026-04-07 16:18:44 -0400

    Added todo-analysis for real

--- todo-analysis.py
+++ todo-analysis.py
@@ -1,7 +1,22 @@
 # iterate through every difficulty dir, every problem dir
 
-# analyze time and space complexity of human, claude, gpt
+# generate a {question_id}_report.md in the filepath
 
-# generate a {question_id}_report.md
+### import current human_gen code for analysis
+### import current claude_gen code for analysis
 
-# place the verdict of analysis
+# Then, analyze time and space complexity of human and claude
+
+### Part 1 - AST - Abstract Tree Syntax
+# Use AST library to statically analyze code. Store result in a NESTED DICT
+
+### Part 2 - Claude
+# Ping claude API to analyze time and space complexity, seeing if it agrees with AST
+
+
+### Part 3 - Merge
+# If they agree... good
+# If they disagree... favor claude, and FLAG the discrepency in the .md
+
+
+# place the verdict of analysis in the report.md that lives in each question dir
